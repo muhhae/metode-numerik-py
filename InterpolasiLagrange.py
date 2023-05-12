aa67ac11ec86d8b54bc98912e3d78b673360f74c
Muhhae <haekal.muhamat@gmail.com>
2023-05-12 18:32:53 +0700

    interpolasi Lagrange

--- InterpolasiLagrange.py
+++ InterpolasiLagrange.py
@@ -10,3 +10,27 @@ def InterpolasiLagrange(x_input, x_list, y_list):
     n = len(x_list)
     return sum( y_list[i] * LagrangeFunction(x_input,i, x_list) for i in range(n))
 
+def StringLagrangeFunction(x_index, x_list):
+    func = ''
+    n = len(x_list)
+    pertama = True
+    
+    for i in range(n):
+        
+        if i != x_index :
+            if pertama == False : func += ' * '
+            func +='((' + 'x' + ' - ' +  str(x_list[i]) + ') / ' + str(x_list[x_index] - x_list[i]) + ')' 
+            pertama = False
+        print(i, func)
+    print('\n')
+    return func
+
+def StringInterpolasiLagrange(x_list, y_list):
+    func = ''
+    n = len(x_list)
+    
+    for i in range(n) :
+        if i > 0 : func += ' + '
+        func += '(' + str(y_list[i]) + ' * ' + StringLagrangeFunction(i, x_list) + ')'
+        print(i, func, '\n')
+    return func
