e0fbdb3d0ebbd22b5160917461c1a3cd848f6134
Muhhae <haekal.muhamat@gmail.com>
2023-05-15 22:02:58 +0700

    Turunan Kontinu Tengah 1,2,3,4

--- TurunanNumerik.py
+++ TurunanNumerik.py
@@ -1,6 +1,14 @@
-def midTurunanDiskrit1(list_x, list_y) :
+def midTurunanDiskrit1(list_x, list_y):
     h = list_x[1] - list_x[0]
-    return (-1 * list_y[0] + list_y[2] ) / (2 * h)  
+    return (-1 * list_y[0] + list_y[2]) / (2 * h)
 
-def midTurunanKontinu1(f,x, h):
-    return (-1 * f(x-h) + f(x+h) ) / (2 * h)
+
+def midTurunanKontinu(f, x, h, turunan_ke):
+    K = [[0, -1, 0, 1, 0], [0, 1, -2, 1, 0], [-1, 2, 0, -2, 1], [1, -4, 6, -4, 1]]
+    pembagi = h ** (turunan_ke)
+    if turunan_ke % 2 != 0 : pembagi *= 2 
+    return (
+        (K[turunan_ke - 1][0] * f(x - 2 * h) +
+         K[turunan_ke - 1][1] * f(x - h) +
+         K[turunan_ke - 1][2] * f(x) + K[turunan_ke - 1][3] * f(x + h) +
+         K[turunan_ke - 1][4] * f(x + 2 * h)) / pembagi )
