5a03314335de140c5f7bd6dedc7d5b23b67896fd
Muhhae <haekal.muhamat@gmail.com>
2023-05-16 12:11:19 +0700

    Integral numerik 1/3 3/8

--- IntegralNumerik.py
+++ IntegralNumerik.py
@@ -1,6 +1,7 @@
 def list():
     print("Trapesium(f, a, b, n=1):")
     print("Titiktengah(f, a, b, n=1):")
+    print("INC2(f, a, b, n=1):")
 
 def Trapesium(f, a, b, n=1):
     itrapesium = lambda f, a, b: abs((b - a) / 2 * (f(b) + f(a)))
@@ -17,5 +18,23 @@ def TitikTengah(f, a, b, n=1):
     while xa < b :
         xb = xa + h
         luas += ititiktengah(f, xa, xb)
+        xa = xb
+    return luas
+
+def INC2(f, a, b, n=1):
+    inc2 = lambda f, a, b:(b-a)/6 * (f(a) + 4 * f((a+b)/2) + f(b))
+    xa = a; h = (b-a) / n;luas = 0
+    for i in range(n):
+        xb = xa + h
+        luas += inc2(f, xa, xb)
+        xa = xb
+    return luas
+
+def INC3(f, a, b, n=1):
+    inc3 =  lambda f,a,b: (b-a)/8 * (f(a)+3*f((b-a)/3)+3*f((2*(b-a))/3)+f(b))
+    xa = a; h = (b-a) / n;luas = 0
+    for i in range(n):
+        xb = xa + h
+        luas += inc3(f, xa, xb)
         xa = xb
     return luas
