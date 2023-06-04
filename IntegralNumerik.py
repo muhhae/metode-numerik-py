6066ceb8faec6b7fae1858ee712ed531ae5e73f9
Muhhae <haekal.muhamat@gmail.com>
2023-06-04 17:12:02 +0700

    Better Output

--- IntegralNumerik.py
+++ IntegralNumerik.py
@@ -8,14 +8,16 @@ def list():
     print("INC4(f, a, b, n=1, out = True)")
 
 def all(f, a, b, n = 4, out = True):
-    print(Trapesium(f, a, b, n, out))
-    print(TitikTengah(f, a, b, n, out))
-    print(INC2(f, a, b, n, out))
-    print(INC3(f, a, b, n, out))
-    print(INC4(f, a, b, n, out))
+    Trapesium(f, a, b, n, out)
+    TitikTengah(f, a, b, n, out)
+    INC2(f, a, b, n, out)
+    INC3(f, a, b, n, out)
+    INC4(f, a, b, n, out)
+    for i in range(2, 6):
+        GaussKuadratur(f, a, b, i, out)
 
 def Trapesium(f, a, b, n=1, out = True):
-    print("\nMetode Trapesium\n")
+    print("\nMetode Trapesium")
     I = lambda a, b: abs((b - a) / 2 * (f(b) + f(a)))
     xa = a; h = (b-a) / n;luas = 0
     while xa < b :
@@ -25,10 +27,11 @@ def Trapesium(f, a, b, n=1, out = True):
             print("(", xa, ",", xb, ") = ", I(xa, xb),sep="")
             print("I = ", luas, "\n")
         xa = xb
+    print("I =", luas)
     return luas
 
 def TitikTengah(f, a, b, n=1, out = True):
-    print("\nMetode Titik Tengah\n")
+    print("\nMetode Titik Tengah")
     I = lambda a, b: abs((b - a) * f((a + b) / 2))
     xa = a; h = (b-a) / n;luas = 0
     while xa < b :
@@ -38,10 +41,11 @@ def TitikTengah(f, a, b, n=1, out = True):
             print("(", xa, ",", xb, ") = ", I(xa, xb),sep="")
             print("I = ", luas, "\n")
         xa = xb
+    print("I =", luas)
     return luas
 
 def INC2(f, a, b, n=1, out = True):
-    print("\nNewton-Cotes Simpson 1/3\n")
+    print("\nNewton-Cotes Simpson 1/3")
     h = (b-a) / n
     d = h / 2
     I = lambda a: 1 / 3 * d * (f(a) + 4 * f(a + d) + f(a + h))
@@ -52,10 +56,11 @@ def INC2(f, a, b, n=1, out = True):
             print("(", a, ",", a + h, ") = ", I(a),sep="")
             print("luas = ", luas, "\n")
         a = a + h
+    print("I =", luas)
     return luas
 
 def INC3(f, a, b, n=1, out = True):
-    print("\nNewton-Cotes Simpson 3/8\n")
+    print("\nNewton-Cotes Simpson 3/8")
     h = (b - a) / n
     d = h/3
     I = lambda a: 3 / 8 * d * (f(a) + 3 * f(a + d) + 3 * f(a + 2 * d) + f(a + h))
@@ -66,10 +71,11 @@ def INC3(f, a, b, n=1, out = True):
             print("(", a, ",", a + h, ") = ", I(a),sep="")
             print("luas = ", luas, "\n")
         a = a + h
+    print("I =", luas)
     return luas
 
 def INC4(f, a, b, n=1, out = True):
-    print("\nNewton-Cotes Boole 2/45\n")
+    print("\nNewton-Cotes Boole 2/45")
     h = (b - a) / n
     d = h/4
     I = lambda a: 2 / 45 * d * ( 7 * f(a) + 32 * f(a + d) + 12 * f(a + 2 * d) + 32 * f(a + 3 * d) + 7 * f(a + h))
@@ -80,9 +86,11 @@ def INC4(f, a, b, n=1, out = True):
             print("(", a, ",", a + h, ") = ", I(a),sep="")
             print("luas = ", luas, "\n")
         a = a + h
+    print("I =", luas)
     return luas
 
 def GaussKuadratur(FungsiAwal, a, b, n=2, out = True):
+    print("\nMetode Gauss Kuadratur Orde", n)
     
     if abs(b-a)<1e-10: return 0
     
@@ -106,9 +114,11 @@ def GaussKuadratur(FungsiAwal, a, b, n=2, out = True):
     total = 0
     n -= 2
     for i in range(n + 2):
-        print("f(", x[n][i], ") * ", c[n][i], " = ", f(x[n][i]) * c[n][i],'\n', sep="")
+        if out :
+            print("f(", x[n][i], ") * ", c[n][i], " = ", f(x[n][i]) * c[n][i],'\n', sep="")
         total += f(x[n][i]) * c[n][i]
     
+    print("I =", total * (b-a) / 2)
     return total * (b-a) / 2
     
     
