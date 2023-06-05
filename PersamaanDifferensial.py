c53d4517ed81a8db17ce77f27ba266ab83d15c33
Muhhae <haekal.muhamat@gmail.com>
2023-06-05 21:54:34 +0700

    Table

--- PersamaanDifferensial.py
+++ PersamaanDifferensial.py
@@ -1,18 +1,34 @@
 from rich import *
+from rich.table import Table
 
-def pdb(f,x,y,h,xb,tipe):
-    if tipe=="euler" :
-        fa=lambda ft,xi,yi,h: yi + ft(xi)* h
-    elif tipe=="heun"  :
-        fa=lambda f,xi,yi,h:yi + h*(f(xi)+ f(xi+h))/2
-    elif tipe=="polygon":
-        fa =lambda f,xi,yi,h:yi + h*f(xi +h/2)
-    elif tipe=="raltson":
-        fa=lambda f,xi,yi,h:yi + h*(f(xi)+2*f(xi+0.75*h))/3                   
-    elif tipe=="kutta" :
-        fa=lambda f,xi,yi,h:yi +  h*(f(xi )+4*f(xi+h/2)+f(xi+h))/6
-        
-    while x<=xb:
-        print(x,y)
-        y=fa(f,x,y,h)
-        x+=h
+def hitung(f, x, y, h, xb, ft):
+    table = Table(title="Hasil Perhitungan")
+    
+    table.add_column("x", justify="left", style="blue")
+    table.add_column("F(x)", justify="left", style="yellow")
+    
+    while x <= xb:
+        table.add_row(str(x), str(y))
+        y = ft(f, x, y, h)
+        x += h
+    print("\n",table)
+
+def euler(f, x, y, h, xb):
+    ft = lambda f, x, y, h: y + f(x) * h
+    hitung(f, x, y, h, xb, ft)
+
+def heun(f, x, y, h, xb):
+    ft = lambda f, x, y, h: y + h*(f(x)+ f(x+h))/2
+    hitung(f, x, y, h, xb, ft)
+    
+def polygon(f, x, y, h, xb):
+    ft = lambda f, x, y, h: y + h*f(x +h/2)
+    hitung(f, x, y, h, xb, ft)
+    
+def raltson(f, x, y, h, xb):
+    ft = lambda f, x, y, h: y + h*(f(x)+2*f(x+0.75*h))/3
+    hitung(f, x, y, h, xb, ft)
+    
+def kutta(f, x, y, h, xb):
+    ft = lambda f, x, y, h: y +  h*(f(x )+4*f(x+h/2)+f(x+h))/6
+    hitung(f, x, y, h, xb, ft)
